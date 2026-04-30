cb2363cb68bf204945ac5bb9360ced3eb4f3d9f9
Lagensha Rupasinghe <lagensharupasinghe47@gmail.com>
2026-04-30 23:33:01 +0530

    Add methods to CustomerService interface for customer management

--- src/main/java/edu/icet/ecom/service/CustomerService.java
+++ src/main/java/edu/icet/ecom/service/CustomerService.java
@@ -1,4 +1,13 @@
 package edu.icet.ecom.service;
 
+import edu.icet.ecom.dto.CustomerDTO;
+
+import java.util.List;
+
 public interface CustomerService {
+    boolean addCustomers(CustomerDTO customerDTO);
+    boolean updateCustomer(CustomerDTO customerDTO);
+    boolean deleteCustomer();
+    List<CustomerDTO> getAll();
+
 }
