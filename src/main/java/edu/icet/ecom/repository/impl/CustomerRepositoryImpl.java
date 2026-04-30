cb01e11f8ce96a7d177f3e55fac2317418c621ba
Lagensha Rupasinghe <lagensharupasinghe47@gmail.com>
2026-04-30 23:34:58 +0530

    Implement CustomerRepository and CustomerService with basic CRUD methods

--- src/main/java/edu/icet/ecom/repository/impl/CustomerRepositoryImpl.java
+++ src/main/java/edu/icet/ecom/repository/impl/CustomerRepositoryImpl.java
@@ -1,4 +1,29 @@
 package edu.icet.ecom.repository.impl;
 
-public class CustomerRepositoryImpl {
+import edu.icet.ecom.dto.CustomerDTO;
+import edu.icet.ecom.repository.CustomerRepository;
+
+import java.util.List;
+
+public class CustomerRepositoryImpl implements CustomerRepository {
+
+    @Override
+    public boolean addCustomers(CustomerDTO customerDTO) {
+        return false;
+    }
+
+    @Override
+    public boolean updateCustomer(CustomerDTO customerDTO) {
+        return false;
+    }
+
+    @Override
+    public boolean deleteCustomer() {
+        return false;
+    }
+
+    @Override
+    public List<CustomerDTO> getAll() {
+        return List.of();
+    }
 }
--- src/main/java/edu/icet/ecom/service/impl/CustomerServiceImpl.java
+++ src/main/java/edu/icet/ecom/service/impl/CustomerServiceImpl.java
@@ -1,4 +1,28 @@
 package edu.icet.ecom.service.impl;
 
-public class CustomerServiceImpl {
+import edu.icet.ecom.dto.CustomerDTO;
+import edu.icet.ecom.service.CustomerService;
+
+import java.util.List;
+
+public class CustomerServiceImpl implements CustomerService {
+    @Override
+    public boolean addCustomers(CustomerDTO customerDTO) {
+        return false;
+    }
+
+    @Override
+    public boolean updateCustomer(CustomerDTO customerDTO) {
+        return false;
+    }
+
+    @Override
+    public boolean deleteCustomer() {
+        return false;
+    }
+
+    @Override
+    public List<CustomerDTO> getAll() {
+        return List.of();
+    }
 }
